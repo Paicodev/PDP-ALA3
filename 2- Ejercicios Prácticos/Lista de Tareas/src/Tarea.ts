5f8e40c5e5de3ee7a9a288de122fae0edbf428c3
Pablo Aguilar <paedmaag@gmail.com>
2025-10-19 18:48:04 -0300

    Se corrigieron errores en Tarea al retornar objetos
    Tarea:
    -ahora no retorna mas objetos, sino que se hace referencia a this: iTarea para concatenar de mejor manera para vincular con los prototipos, ya que, antes me daban errores en el gestor de tareas.
    -Cada metodo del prototype usara como parametro a la interfaz de Tarea.
    -Se corrigió la restriccion cuando se definian las dificultades

--- 2- Ejercicios Prácticos/Lista de Tareas/src/Tarea.ts
+++ 2- Ejercicios Prácticos/Lista de Tareas/src/Tarea.ts
@@ -1,55 +1,55 @@
-import { ITarea } from "./ITarea";
+import { iTarea } from "./iTarea";
 
-//Definimos la funcion constructora Tarea, que recibe los datos de la interfaz ITarea y devuelve un objeto de tipo ITarea
-function Tarea(titulo: string, descripcion: string, estado: string, dificultad: number, vencimiento: Date | null): ITarea {
-    //Creamos el objeto ITarea con las propiedades recibidas y las fechas de creacion y edicion actuales
-const obj: ITarea = {
-    titulo,
-    descripcion,
-    estado,
-    dificultad,
-    vencimiento,
-    creacion: new Date(),
-    edicion: new Date(),
+//Definimos la funcion constructora Tarea, que recibe a this de tipo iTarea y los parametros necesarios para crear una tarea
+function Tarea(this: iTarea, titulo: string, descripcion: string, estado: string, dificultad: number, vencimiento: Date | null){
+    
+{
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.estado = estado;
+    this.dificultad = dificultad;
+    this.vencimiento = vencimiento;
+    this.creacion = new Date();
+    this.edicion = new Date();
 };
-//Devolvemos el objeto creado, esto asegura que el objeto tenga toda la estructura definida en la interfaz ITarea
-return obj;
+
 }
 
 /*Metodos agregados al prototipo de Tarea
   *Aqui hago uso de prototipos para agregar metodos al objeto Tarea. 
   *Cada objeto de tipo Tarea podra usar estos metodos para modificar sus propiedades o mostrar informacion.
   *Tarea debe definirse como any para evitar errores de tipos al agregar metodos al prototipo en TypeScript.
+  *Cada function devuelve this: iTarea cuando es un metodo de edicion, para permitir encadenar llamadas si se desea.
   *Todos los metodos son de tipo function para mantener el contexto de 'this' correcto.
   */
-(Tarea as any).prototype.editarTitulo = function (nuevoTitulo: string){
+(Tarea as any).prototype.editarTitulo = function (this: iTarea, nuevoTitulo: string){
     this.titulo = nuevoTitulo || this.titulo;
     this.edicion = new Date();
 }; 
 
-(Tarea as any).prototype.editarDesc = function (nuevaDesc: string){
+(Tarea as any).prototype.editarDesc = function (this: iTarea, nuevaDesc: string){
     this.descripcion = nuevaDesc || this.descripcion;
     this.edicion = new Date();
 };
 
-(Tarea as any).prototype.editarEstado = function (nuevoEstado: string){
+(Tarea as any).prototype.editarEstado = function (this: iTarea, nuevoEstado: string){
     this.estado = nuevoEstado || this.estado;
     this.edicion = new Date();
 };
 
-(Tarea as any).prototype.editarDif = function (nuevaDif: number){
-    if ( nuevaDif >= 1 && nuevaDif <= 5 ){
+(Tarea as any).prototype.editarDif = function (this: iTarea, nuevaDif: number){
+    if ( nuevaDif >= 1 && nuevaDif <= 3 ){
         this.dificultad = nuevaDif;
         this.edicion = new Date();
     }
 };
 
-(Tarea as any).prototype.editarVenc = function (nuevoVenc: Date | null){
+(Tarea as any).prototype.editarVenc = function (this: iTarea, nuevoVenc: Date | null){
     this.vencimiento = nuevoVenc || this.vencimiento;
     this.edicion = new Date();
 };
 
-(Tarea as any).prototype.getDifEstrellas = function (): string {
+(Tarea as any).prototype.getDifEstrellas = function (this:iTarea): string {
     //Segun lo que vale this.dificultad, devuelve una cadena con estrellas representando la dificultad
     switch (this.dificultad){
         case 1: 
